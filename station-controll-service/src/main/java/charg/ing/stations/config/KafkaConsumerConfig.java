3020b9b06b4e33e0969403435859ea724b26fc8e
Bagyshan <Bagishan01@gmail.com>
2025-10-17 20:01:27 +0600

    add 2 services to docker compose file

--- station-controll-service/src/main/java/charg/ing/stations/config/KafkaConsumerConfig.java
+++ station-controll-service/src/main/java/charg/ing/stations/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ package charg.ing.stations.config;
 import charg.ing.stations.dto.StationCreateEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -23,10 +24,14 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "station-controller-service-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
